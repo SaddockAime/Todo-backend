a75f5e219fc04b864983eb298e8d863c8ce2aab6
SaddockAime <aimegetz@gmail.com>
2024-04-19 12:07:12 +0200

    server file

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,30 @@
-import express from 'express';
-import http from 'http';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import compression from 'compression';
-import cors from 'cors';
-import mongoose from 'mongoose';
-
+import express, { Request, Response } from 'express';
 import allRoutes from "./routes/allRoutes"
+import cors from 'cors';
+import dotenv from 'dotenv';
+import swaggerSetup from '../swaggerConfig';
+import './database/config/database'
 
-//Express application
 const app = express();
+app.use(express.json());
+app.use(cors());
+dotenv.config();
 
-app.use(cors({
-  credentials: true,
-}));
+app.use("/api", allRoutes);
 
-app.use(compression());
-app.use(bodyParser.json());
+swaggerSetup(app);
 
-const server = http.createServer(app);
-
-const PORT:number = parseInt(process.env.PORT!) || 5050;
-
-server.listen(PORT, () => {
-    console.log('server running on http://localhost:5050/');
+app.get('/test', (req: Request, res: Response) => {
+    res.send('Hello');
 });
 
-const MONGO_URL = 'mongodb+srv://aimegetz:4ANkqZh1vkvm1p9j@cluster0.7d6ceay.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+app.get("/", (req: Request, res: Response) => {
+    res.json({ message: "Welcome To TODO BackEnd" });
+});
 
-mongoose.Promise = Promise;
-mongoose.connect(MONGO_URL);
-mongoose.connection.on('error', (error: Error) => console.log(error));
+const PORT = process.env.PORT || 5050;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
-app.use('/api/', allRoutes);
+export default app;
