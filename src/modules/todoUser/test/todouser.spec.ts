0c8fe66a70a9dcb557c0e726761e3f51e1196217
SaddockAime <aimegetz@gmail.com>
2024-04-21 18:30:17 +0200

    todo user tests

--- src/modules/todoUser/test/todouser.spec.ts
+++ src/modules/todoUser/test/todouser.spec.ts
@@ -5,39 +5,16 @@ import chai, { expect } from "chai";
 chai.use(chaiHttp);
 const router = () => chai.request(app);
 
+
+// loging which will retun the token
+
+let token: any  = '';
+let userId: any = '';
+
+
+
+
 describe("todo user test cases", () => {
-
-  // Test for view users
-  it("Should be able to get Users", (done) => {
-    router()
-      .get("/api/todousers/viewusers")
-      .end((error, response) => {
-        expect(response).to.have.status(200);
-        expect(response.body).to.be.a("object");
-        expect(response.body).to.have.property("data");
-        expect(response.body.message).to.be.a("string");
-        done(error);
-      });
-  });
-
-  // Test for signup
-  it("Should be able to sign up a new user", (done) => {
-    router()
-      .post("/api/todousers/signup")
-      .send({
-        username: "Bado",
-        email: "bado@gmail.com",
-        password: "Bado_2000"
-      })
-      .end((error, response) => {
-        expect(response).to.have.status(200);
-        expect(response.body).to.be.an("object");
-        expect(response.body).to.have.property("data");
-        expect(response.body.data).to.have.property("username", "Bado");
-        expect(response.body.data).to.have.property("email", "bado@gmail.com");
-        done(error);
-      });
-  });
 
   // Test for login
   it("Should be able to log in an existing user", (done) => {
@@ -47,10 +24,46 @@ describe("todo user test cases", () => {
         email: "saddock@gmail.com",
         password: "Saddock_2000"
       })
-      .end((error, response) => {
+      .end((error, response: any) => {
         expect(response).to.have.status(200);
         expect(response.body).to.be.an("object");
         expect(response.body).to.have.property("data");
+        token = response._body.data.token;
+        done(error);
+      });
+  });
+
+  // Test for view users
+  it("Should be able to get Users", (done) => {
+    router()
+      .get("/api/todousers/viewusers")
+      .set("Authorization", `Bearer ${token}`)
+      .end((error, response) => {
+        expect(response).to.have.status(200);
+        expect(response.body).to.be.a("object");
+        expect(response.body).to.have.property("data");
+        expect(response.body.message).to.be.a("string");
+        done(error);
+      });
+  });
+
+  //Test for signup
+  it("Should be able to sign up a new user", (done) => {
+    router()
+      .post("/api/todousers/signup")
+      .send({
+        username: "Dodo",
+        email: "dodo@gmail.com",
+        password: "dodo_2000"
+      })
+      .end((error, response: any) => {
+        expect(response).to.have.status(200);
+        expect(response.body).to.be.an("object");
+        expect(response.body).to.have.property("data");
+        expect(response.body.data).to.have.property("username", "Dodo");
+        expect(response.body.data).to.have.property("email", "dodo@gmail.com");
+        userId = response._body.data._id
+        console.log(userId)
         done(error);
       });
   });
@@ -58,14 +71,15 @@ describe("todo user test cases", () => {
   // Test for single user
   it("Should be able to get a single user by ID", (done) => {
     // Replace 'userId' with an actual user ID from your database
-    const userId = "66223c17b5b4ba0e385b44ee";
     router()
       .get(`/api/todousers/singleUser/${userId}`)
+      .set("Authorization", `Bearer ${token}`)
       .end((error, response) => {
         expect(response).to.have.status(200);
         expect(response.body).to.be.an("object");
         expect(response.body).to.have.property("data");
         expect(response.body.data).to.have.property("_id", userId);
+        console.log(error)
         done(error);
       });
   });
@@ -73,15 +87,15 @@ describe("todo user test cases", () => {
   // Test for editing user
   it("Should be able to edit a user", (done) => {
     // Replace 'userId' with an actual user ID from your database
-    const userId = "66216740052667e6993c8045";
     router()
       .put(`/api/todousers/editUser/${userId}`)
-      .send({ username: "Aime" })
+      .set("Authorization", `Bearer ${token}`)
+      .send({ username: "Dodo" })
       .end((error, response) => {
         expect(response).to.have.status(200);
         expect(response.body).to.be.an("object");
         expect(response.body.message).to.be.a("string");
-        expect(response.body.data).to.have.property("username", "Aime");
+        expect(response.body.data).to.have.property("username", "Dodo");
         done(error);
       });
   });
@@ -89,9 +103,9 @@ describe("todo user test cases", () => {
   // Test for deleting a user
   it("Should be able to delete a user by ID", (done) => {
     // Replace 'userId' with an actual user ID from your database
-    const userId = "66223967a924908c215db02c";
     router()
       .delete(`/api/todousers/deleteUser/${userId}`)
+      .set("Authorization", `Bearer ${token}`)
       .end((error, response) => {
         expect(response).to.have.status(200);
         expect(response.body).to.be.an("object");
